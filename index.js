a4cbea91c1b68fb26eb24538b52fd26cee5a5fe2
Michael Pacyga <mpacyga@discover.com>
2019-02-25 11:45:18 -0600

    fsdjfk

--- index.js
+++ index.js
@@ -203,7 +203,7 @@ app.post('/notifications/options/post', function(request,response) {
 });
 
 app.get('/notifications/options/gets', function(request,response) {
-  connection.query('SELECT Name, Description, Date FROM Notifications FORMAT(Date, MM/DD/YYYY)', function (error, results, fields) {
+  connection.query('SELECT Name, Description, Date(DATE) FROM Notifications', function (error, results, fields) {
     if(error) {
       response.json({Metrics_get: "failed"});
     }
