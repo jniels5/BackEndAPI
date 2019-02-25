8e0f5ab4f5857d77a097282ec8458236f1c361ca
Michael Pacyga <mpacyga@discover.com>
2019-02-25 12:15:09 -0600

    asdada

--- index.js
+++ index.js
@@ -203,7 +203,7 @@ app.post('/notifications/options/post', function(request,response) {
 });
 
 app.get('/notifications/options/gets', function(request,response) {
-  connection.query('SELECT Name, Description, Date FROM Notifications', function (error, results, fields) {
+  connection.query('SELECT Name, Description, Convert(varchar(10), GETDATE(),120)  FROM Notifications', function (error, results, fields) {
     if(error) {
       response.json({Metrics_get: "failed"});
     }
