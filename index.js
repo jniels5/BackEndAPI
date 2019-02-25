e766ea079d0144427ae325ec1f61df107802d757
Michael Pacyga <mpacyga@discover.com>
2019-02-25 11:09:25 -0600

    changed notfication select statement

--- index.js
+++ index.js
@@ -203,7 +203,7 @@ app.post('/notifications/options/post', function(request,response) {
 });
 
 app.get('/notifications/options/gets', function(request,response) {
-  connection.query('SELECT Name, Description, CONVERT(VARCHAR(12), date, 107) AS [Mon, DD, YYYY] FROM Notifications', function (error, results, fields) {
+  connection.query('SELECT Name, Description, Date(Date) FROM Notifications', function (error, results, fields) {
     if(error) {
       response.json({Metrics_get: "failed"});
     }
