f3c1f7c5756ec70ff85f31061b0c8a6dfee57852
Jeremy Nielson <jniels5@discover.com>
2019-02-28 12:05:59 -0600

    Im a genuis

--- drop.sql
+++ drop.sql
@@ -1,13 +1,34 @@
-DROP TABLE IF EXISTS Projects;
-DROP TABLE IF EXISTS TeamMembers;
-DROP TABLE IF EXISTS Teams;
-DROP TABLE IF EXISTS Semesters;
-DROP TABLE IF EXISTS Role;
-DROP TABLE IF EXISTS Members;
-DROP TABLE IF EXISTS Assets;
-DROP TABLE IF EXISTS Metrics;
-DROP TABLE IF EXISTS Preferences;
-DROP TABLE IF EXISTS Notifications;
-DROP TABLE IF EXISTS AccountLabs;
-DROP TABLE IF EXISTS Labs;
-DROP TABLE IF EXISTS Accounts;
+INSERT INTO Members(FirstName, LastName, Gender, GradYear, Email, AssetID, LabID) VALUES
+('Jeremy', 'Nielson', 'Male', 'SP19', 'saktirshinu@gmail.com', 1, 1),
+('Sean', 'Wallace', 'Male', 'SP20', 'walis846@gmail.com', 2, 1),
+('Cole', 'Braswell', 'Male', 'SP20', 'braswellcole@outlook.com', 3, 1),
+('Cody', 'Knight', 'Male', 'SP19', 'pulse14@live.com', 4, 1),
+('Michael', 'Pacyga', 'Male', 'SP20', 'michagy@gmail.com', 5, 1);
+
+INSERT INTO Role(Type, Status, Description, Date, MemberID) VALUES
+('Intern', 'Full-time hire', 'Has accepted full time position at DFS. Expected to work following graduation', '2019-2-12', 1),
+('Intern', 'Active', 'Working second semester at code_orange', '2019-2-14', 2),
+('Intern', 'Active', 'First semester at code_orange', '2019-2-14', 3),
+('Intern', 'Active', 'First semester at code_orange. Graduates Spring 2019', '2019-2-14', 4),
+('Intern', 'Active', 'First semester at code_orange', '2019-2-14', 5);
+
+INSERT INTO Semesters(Semester, Year, MemberID) VALUES
+('Fall', 2018, 1),
+('Spring', 2019, 1),
+('Fall', 2018, 2),
+('Spring', 2019, 2),
+('Spring', 2019, 3),
+('Spring', 2019, 4);
+
+INSERT INTO Teams(TeamName, PO, Mentor, Semester, LabID) VALUES
+('Team11', 'Mike Brenner', 'Jeremy Nielson', 'SP19', 1);
+
+INSERT INTO TeamMembers(TeamID, MemberID) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 5);
+
+INSERT INTO Projects(Name, Type, Description, TeamID) VALUES
+('Applicant Tracking & Dashboard', 'Website', 'Keep track of all applicants, interns, and hires from the code_orange project', 1);
--- tables.sql
+++ tables.sql
@@ -141,37 +141,3 @@ INSERT INTO Assets(AssetTag, Type, Description, IsImaged, LabID) VALUES
 ( 20100464, 'Laptop', 'Apple MacBook Pro', 1, 1),
 ( 20100217, 'Laptop', 'Apple MacBook Pro', 1, 1),
 ( 20108969, 'Laptop', 'Apple MacBook Pro', 1, 1);
-
-INSERT INTO Members(FirstName, LastName, Gender, GradYear, Email, AssetID, LabID) VALUES
-('Jeremy', 'Nielson', 'Male', 'SP19', 'saktirshinu@gmail.com', 1, 1),
-('Sean', 'Wallace', 'Male', 'SP20', 'walis846@gmail.com', 2, 1),
-('Cole', 'Braswell', 'Male', 'SP20', 'braswellcole@outlook.com', 3, 1),
-('Cody', 'Knight', 'Male', 'SP19', 'pulse14@live.com', 4, 1),
-('Michael', 'Pacyga', 'Male', 'SP20', 'michagy@gmail.com', 5, 1);
-
-
-INSERT INTO Role(Type, Status, Description, Date, MemberID) VALUES
-('Intern', 'Full-time hire', 'Has accepted full time position at DFS. Expected to work following graduation', '2019-2-12', 1),
-('Intern', 'Active', 'Working second semester at code_orange', '2019-2-14', 2),
-('Intern', 'Active', 'First semester at code_orange', '2019-2-14', 3),
-('Intern', 'Active', 'First semester at code_orange. Graduates Spring 2019', '2019-2-14', 4);
-
-INSERT INTO Semesters(Semester, Year, MemberID) VALUES
-('Fall', 2018, 1),
-('Spring', 2019, 1),
-('Fall', 2018, 2),
-('Spring', 2019, 2),
-('Spring', 2019, 3),
-('Spring', 2019, 4);
-
-INSERT INTO Teams(TeamName, PO, Mentor, Semester, LabID) VALUES
-('Team11', 'Mike Brenner', 'Jeremy Nielson', 'SP19', 1);
-
-INSERT INTO TeamMembers(TeamID, MemberID) VALUES
-(1, 1),
-(1, 2),
-(1, 3),
-(1, 4);
-
-INSERT INTO Projects(Name, Type, Description, TeamID) VALUES
-('Applicant Tracking & Dashboard', 'Website', 'Keep track of all applicants, interns, and hires from the code_orange project', 1);
