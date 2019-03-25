55a0877650c5be6afd4381de0dd33690d48f9b33
Jeremy Nielson <jniels5@discover.com>
2019-03-25 13:53:54 -0500

    Teams Query w/ Modular semester

--- index.js
+++ index.js
@@ -387,7 +387,12 @@ app.post('/checkin', function(request,response) {
 
 // Select Info for Teams page
 app.get('/select/table/Members/team/SP19/:team', function(request,response) {
-  connection.query('SELECT Members.FirstName, Members.LastName, Teams.TeamName, Projects.Name, Projects.Description, Projects.Paragraph, Projects.FrontEnd, Projects.Backend, Projects.RDS FROM Members JOIN TeamMembers ON TeamMembers.MemberID = Members.MemberID JOIN Teams ON Teams.TeamID = TeamMembers.TeamID JOIN Projects ON Projects.TeamID = Teams.TeamID WHERE Teams.TeamID = ' + request.params.team + ' AND Teams.Semester = "SP19" ORDER BY FirstName', function (error, results, fields) {
+  connection.query('SELECT Members.FirstName, Members.LastName, Teams.TeamName, Teams.Semester, Teams.TeamNumber, ' +
+                    'Projects.Name, Projects.Description, Projects.Paragraph, Projects.FrontEnd, Projects.Backend, ' +
+                    'Projects.RDS FROM Members JOIN TeamMembers ON TeamMembers.MemberID = Members.MemberID ' +
+                    'JOIN Teams ON Teams.TeamID = TeamMembers.TeamID JOIN Projects ON Projects.TeamID = Teams.TeamID ' +
+                    'WHERE Teams.TeamID = ' + request.params.team +
+                    ' AND Teams.Semester = "SP19" ORDER BY FirstName', function (error, results, fields) {
         if(error) {
             response.json({select_status: "failed"});
         }
@@ -397,8 +402,13 @@ app.get('/select/table/Members/team/SP19/:team', function(request,response) {
   });
 });
 
-app.get('/select/table/Members/team/FA18/:team', function(request,response) {
-  connection.query('SELECT Members.FirstName, Members.LastName, Teams.TeamName, Projects.Name, Projects.Description, Projects.Paragraph, Projects.FrontEnd, Projects.Backend, Projects.RDS FROM Members JOIN TeamMembers ON TeamMembers.MemberID = Members.MemberID JOIN Teams ON Teams.TeamID = TeamMembers.TeamID JOIN Projects ON Projects.TeamID = Teams.TeamID WHERE Teams.TeamID = ' + request.params.team + ' AND Teams.Semester = "FA18" ORDER BY FirstName', function (error, results, fields) {
+app.get('/select/table/Members/team/:team/:semester', function(request,response) {
+  connection.query('SELECT Members.FirstName, Members.LastName, Teams.TeamName, Teams.Semester, Teams.TeamNumber, ' +
+                    'Projects.Name, Projects.Description, Projects.Paragraph, Projects.FrontEnd, Projects.Backend, ' +
+                    'Projects.RDS FROM Members JOIN TeamMembers ON TeamMembers.MemberID = Members.MemberID ' +
+                    'JOIN Teams ON Teams.TeamID = TeamMembers.TeamID JOIN Projects ON Projects.TeamID = Teams.TeamID ' +
+                    'WHERE Teams.TeamID = ' + request.params.team +
+                    ' AND Teams.Semester = ' + request.params.semester + ' ORDER BY FirstName', function (error, results, fields) {
         if(error) {
             response.json({select_status: "failed"});
         }
