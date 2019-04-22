0dcce34e46e2a191c36f75c4b0fb1a1a0b369640
Jeremy Nielson <jniels5@discover.com>
2019-04-22 14:14:22 -0500

    Added mobile devices

--- update.sql
+++ update.sql
@@ -1,13 +1,15 @@
 SET foreign_key_checks = 0;
 
-DELETE FROM Role WHERE RoleID = 114;
-DELETE FROM Role WHERE RoleID = 93;
-DELETE FROM Role WHERE RoleID = 123;
-DELETE FROM Role WHERE RoleID = 112;
-
-DELETE FROM Members WHERE MemberID = 114;
-DELETE FROM Members WHERE MemberID = 93;
-DELETE FROM Members WHERE MemberID = 123;
-DELETE FROM Members WHERE MemberID = 112;
+INSERT INTO Assets (AssetID, Type, Description, IsImaged, LabID) VALUES
+(50005573, "Mobile Device", "Samsung Galaxy S8", 1, 1),
+(50005574, "Mobile Device", "Samsung Galaxy S8", 1, 1),
+(50005575, "Mobile Device", "Samsung Galaxy S8", 1, 1),
+(20110985, "Mobile Device", "Samsung Galaxy Tab A", 1, 1),
+(10000001, "Mobile Device", "Samsung Galaxy Note 10.1", 1, 1),
+(50005576, "Mobile Device", "iPhone 8", 1, 1),
+(50005577, "Mobile Device", "iPhone 8", 1, 1),
+(50005578, "Mobile Device", "iPhone 8", 1, 1),
+(20082954, "Mobile Device", "iPad", 1, 1),
+(20110998, "Mobile Device", "iPad", 1, 1);
 
 SET foreign_key_checks = 1;
