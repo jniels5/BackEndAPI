35bef1617e35d0129ad6ed5c32d68fabc6ddbb7b
Jeremy Nielson <jniels5@discover.com>
2019-04-24 16:14:52 -0500

    Final projects update

--- index.js
+++ index.js
@@ -863,7 +863,9 @@ app.post('/checkin', function(request,response) {
 app.get('/select/teamPageData/:semester', function(request,response) {
   connection.query('SELECT Teams.TeamName, Teams.Semester, Teams.TeamNumber, Teams.PhotoPath,' +
                     'Projects.Name, Projects.Description, Projects.Paragraph, Projects.FrontEnd, Projects.Backend, ' +
-                    'Projects.RDS FROM Teams JOIN Projects ON Projects.TeamID = Teams.TeamID ' +
+                    'Projects.RDS FROM Teams ' +
+                    'JOIN TeamProjects ON Teams.TeamID = TeamProjects.TeamID ' +
+                    'JOIN Projects ON Projects.ProjectID = TeamProjects.ProjectID ' +
                     ' AND Teams.Semester = "' + request.params.semester + '"', function (error, results, fields) {
         if(error) {
             response.json({select_status: "failed"});
--- update.sql
+++ update.sql
@@ -1,35 +1,5 @@
 SET foreign_key_checks = 0;
 
-ALTER TABLE `Projects` DROP INDEX `TeamID`;
-
-CREATE TABLE TeamProjects
-(TeamID int NOT NULL,
-ProjectID int NOT NULL,
-FOREIGN KEY (TeamID) REFERENCES Teams(TeamID),
-FOREIGN KEY (ProjectID) REFERENCES Projects(ProjectID),
-UNIQUE (TeamID, ProjectID));
-
-INSERT INTO TeamProjects (TeamID, ProjectID) VALUES
-(1,1),
-(2,2),
-(3,3), (10,3),
-(4,11), (11,11),
-(5,5),
-(6,6),
-(7,7), (14,7),
-(8,8),
-(9,9),
-(12,12),
-(13,13),
-(15,15),
-(16,16),
-(17,17),
-(18,18),
-(19,19),
-(20,20);
-
-DELETE FROM Projects WHERE ProjectID = 10;
-DELETE FROM Projects WHERE ProjectID = 11;
-DELETE FROM Projects WHERE ProjectID = 14;
+ALTER TABLE Projects DROP TeamID;
 
 SET foreign_key_checks = 1;
