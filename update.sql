45afee229bb944a2c36e892719c511def6fffce0
Jeremy Nielson <jniels5@discover.com>
2019-04-24 15:23:14 -0500

    hope this works...

--- update.sql
+++ update.sql
@@ -1,15 +1,36 @@
 SET foreign_key_checks = 0;
 
-INSERT INTO Assets (AssetID, Type, Description, IsImaged, LabID) VALUES
-(50005573, "Mobile Device", "Samsung Galaxy S8", 1, 1),
-(50005574, "Mobile Device", "Samsung Galaxy S8", 1, 1),
-(50005575, "Mobile Device", "Samsung Galaxy S8", 1, 1),
-(20110985, "Mobile Device", "Samsung Galaxy Tab A", 1, 1),
-(10000001, "Mobile Device", "Samsung Galaxy Note 10.1", 1, 1),
-(50005576, "Mobile Device", "iPhone 8", 1, 1),
-(50005577, "Mobile Device", "iPhone 8", 1, 1),
-(50005578, "Mobile Device", "iPhone 8", 1, 1),
-(20082954, "Mobile Device", "iPad", 1, 1),
-(20110998, "Mobile Device", "iPad", 1, 1);
+ALTER TABLE Projects DROP FOREIGN KEY TeamID;
+ALTER TABLE Projects DROP INDEX TeamID;
+
+CREATE TABLE TeamProjects
+(TeamID int NOT NULL,
+ProjectID int NOT NULL,
+FOREIGN KEY (TeamID) REFERENCES Teams(TeamID),
+FOREIGN KEY (ProjectID) REFERENCES Projects(ProjectID),
+UNIQUE (TeamID, ProjectID));
+
+INSERT INTO TeamProjects (TeamID, ProjectID) VALUES
+(1,1),
+(2,2),
+(3,3), (10,3),
+(4,11), (11,11),
+(5,5),
+(6,6),
+(7,7), (14,7),
+(8,8),
+(9,9),
+(12,12),
+(13,13),
+(15,15),
+(16,16),
+(17,17),
+(18,18),
+(19,19),
+(20,20),
+
+DELETE FROM Projects WHERE ProjectID = 10;
+DELETE FROM Projects WHERE ProjectID = 11;
+DELETE FROM Projects WHERE ProjectID = 14;
 
 SET foreign_key_checks = 1;
