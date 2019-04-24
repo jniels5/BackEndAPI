7c32eb7c922ca05a8f65f41a0fe0ce464c32ded5
Jeremy Nielson <jniels5@discover.com>
2019-04-24 17:14:19 -0500

    more fixs

--- update.sql
+++ update.sql
@@ -3,4 +3,7 @@ SET foreign_key_checks = 0;
 INSERT INTO TeamProjects (TeamID, ProjectID) VALUES
 (4,4), (11,4);
 
+DELETE TeamProjects WHERE ProjectID = 11 AND TeamID = 4;
+DELETE TeamProjects WHERE ProjectID = 11 AND TeamID = 11;
+
 SET foreign_key_checks = 1;
