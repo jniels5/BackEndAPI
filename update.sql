91fd456055e3cb48fe679e502a4dd9ef964ce068
Jeremy Nielson <jniels5@discover.com>
2019-05-01 10:18:49 -0500

    AH

--- update.sql
+++ update.sql
@@ -1,6 +1,10 @@
 SET foreign_key_checks = 0;
 
-ALTER TABLE Members
-ADD COLUMN PhoneNum VARCHAR(15) AFTER WorkEmail;
+DELETE FROM Role WHERE RoleID = 139;
+DELETE FROM Role WHERE RoleID = 140;
+DELETE FROM Role WHERE RoleID = 141;
+DELETE FROM Members WHERE MemberID = 139;
+DELETE FROM Members WHERE MemberID = 140;
+DELETE FROM Members WHERE MemberID = 141;
 
 SET foreign_key_checks = 1;
