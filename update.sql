582f357a980d2c2ccede8f380a8b68d18f69ee7f
Jeremy Nielson <jniels5@discover.com>
2019-05-01 10:28:19 -0500

    Dab

--- update.sql
+++ update.sql
@@ -1,10 +1,8 @@
 SET foreign_key_checks = 0;
 
-DELETE FROM Role WHERE RoleID = 139;
-DELETE FROM Role WHERE RoleID = 140;
-DELETE FROM Role WHERE RoleID = 141;
-DELETE FROM Members WHERE MemberID = 139;
-DELETE FROM Members WHERE MemberID = 140;
-DELETE FROM Members WHERE MemberID = 141;
+DELETE FROM Role WHERE RoleID = 142;
+DELETE FROM Members WHERE MemberID = 142;
+DELETE FROM Role WHERE RoleID = 143;
+DELETE FROM Members WHERE MemberID = 143;
 
 SET foreign_key_checks = 1;
