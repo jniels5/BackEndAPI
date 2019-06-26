08cd866da2c6d46a92eb7c053e8441f3fdabdb82
DiscoverDaniel <danielomalley@discover.com>
2019-06-26 22:17:06 +0000

    added nodemailer emails on some api cals

--- index.js
+++ index.js
@@ -8,6 +8,33 @@ var fs = require('fs');
 
 var mes, res;
 var conflicts;
+//--------------- Email Begin----------------
+var nodemailer = require("nodemailer");
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'ChangeLogWarden@gmail.com',
+    pass: '777cH0!d'
+  }
+});
+
+var mailOptions = {
+  from: 'ChangeLogWarden@gmail.com',
+  to: 'danielomalley@discover.com',
+  subject: 'Reservations Page Email',
+  text: 'BIG NYA BOOM BOOM BING'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if(error){
+    res.json(null)
+  }
+  else{
+    res.json({email: 'sent'})
+  }
+})
+//-------------- Email End ---------------------
 
 var whitelist = [
   'localhost:3000/',
@@ -940,6 +967,23 @@ app.get('/remove/reservation/:rID', function(request,response) {
             });
         }
         else {
+          // EMAIL TOKEN
+           var mailOptions = {
+            from: 'ChangeLogWarden@gmail.com',
+            to: 'danielomalley@discover.com',
+            subject: 'code_orange Reservations',
+            text: 'Your Reservation has been DELETED.  Check the reservations page for more deets.'
+          };
+          
+          transporter.sendMail(mailOptions, function(error, info){
+            if(error){
+              response.json(null)
+            }
+            else {
+              response.json({email: "sent"})
+            }
+          });
+          //END EMAIL TOKEN
             response.json({
               remove_status: "success",
             });
@@ -950,7 +994,6 @@ app.get('/remove/reservation/:rID', function(request,response) {
 // Delete all reservations (Deprecated)
 app.get('/delete/reservations', function(request,response) {
   //used in connection.query
-
   connection.query('DELETE FROM Reservations', function (error, results, fields) {
         if(error) {
             response.json({
@@ -978,6 +1021,23 @@ app.post('/update/reserve', function(request,response) {
             });
         }
         else {
+          //EMAIL TOKEN
+          var mailOptions = {
+            from: 'ChangeLogWarden@gmail.com',
+            to: 'danielomalley@discover.com',
+            subject: 'code_orange Reservations',
+            text: 'Your Reservation has been updated.  Check the reservations page for more deets.'
+          };
+          
+          transporter.sendMail(mailOptions, function(error, info){
+            if(error){
+              response.json(null)
+            }
+            else {
+              response.json({email: "sent"})
+            }
+          });
+          //END EMAIL TOKEN
             response.json({
               update_status: "success",
             });
@@ -1047,6 +1107,23 @@ app.post('/insert/reserve/', function(request,response) {
               });
             }
           else {
+            //EMAIL TOKEN
+          var mailOptions = {
+            from: 'ChangeLogWarden@gmail.com',
+            to: 'danielomalley@discover.com',
+            subject: 'code_orange Reservations',
+            text: 'Your Reservation has been MADE.  Check the reservations page for more deets.'
+          };
+          
+          transporter.sendMail(mailOptions, function(error, info){
+            if(error){
+              response.json(null)
+            }
+            else {
+              response.json({email: "sent"})
+            }
+          });
+          // END EMAIL TOKEN
             response.json({
               checkin_status: "success",
               conflict_count: conflicts,
@@ -1096,7 +1173,19 @@ app.get('/runfile/:file', function(request,response) {
   response.json({runfile_status: "Success"});
 });
 
-
+//funciton is used to send an email to the userpool
+app.get('/getmail', function(request,response){
+  //EMAIL TOKEN
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      response.json(null)
+    }
+    else{
+      response.json({email: "sent"})
+    }
+  })
+});
+  //END EMAIL TOKEN
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'));
