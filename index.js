29b54f832b469c65393d8b36df5694ff42b417d6
DiscoverDaniel <danielomalley@discover.com>
2019-07-02 23:10:06 +0000

    delete emails

--- index.js
+++ index.js
@@ -979,7 +979,7 @@ app.get('/remove/reservation/:rID', function(request,response) {
         }
   });
   
-    var emailQuery = "select WorkEmail, Reservations.Date, Start, End, Reservations.RoomID, Teams.TeamNumber from Members INNER JOIN TeamMembers ON TeamMembers.MemberID=Members.MemberID INNER JOIN Teams ON TeamMembers.TeamID=Teams.TeamID INNER JOIN Reservations ON Teams.TeamNumber=Reservations.TeamID WHERE Reservations.ReserveID=513 AND Teams.TeamID=(SELECT MAX(Teams.TeamID)   FROM Teams INNER JOIN Reservations ON Teams.TeamNumber=Reservations.TeamID WHERE Reservations.ReserveID=513 GROUP BY Teams.TeamID ORDER BY Teams.TeamID DESC LIMIT 1);"
+    var emailQuery = "select WorkEmail, Reservations.Date, Start, End, Reservations.RoomID, Teams.TeamNumber from Members INNER JOIN TeamMembers ON TeamMembers.MemberID=Members.MemberID INNER JOIN Teams ON TeamMembers.TeamID=Teams.TeamID INNER JOIN Reservations ON Teams.TeamNumber=Reservations.TeamID WHERE Reservations.ReserveID="+mysql.escape(request.params.rID)+" AND Teams.TeamID=(SELECT MAX(Teams.TeamID)   FROM Teams INNER JOIN Reservations ON Teams.TeamNumber=Reservations.TeamID WHERE Reservations.ReserveID="+mysql.escape(request.params.rID)+" GROUP BY Teams.TeamID ORDER BY Teams.TeamID DESC LIMIT 1);"
   connection.query(emailQuery, function(error, results, fields) {
       if(error){
            response.json({
