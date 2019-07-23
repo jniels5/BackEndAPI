f63a67444813c4f9ade88cb6f9dff44aae2edfc8
Jeremy Nielson <jniels5@discover.com>
2019-07-23 13:56:44 -0500

    testing rerouting

--- index.js
+++ index.js
@@ -92,6 +92,14 @@ connection.connect(function(err) {
   console.log('Connected to database.');
 });
 
+// Pull in route files
+var PCFRoutes = require('./routes/PCF');
+var Metrics = require("./routes/Metrics.js");
+
+// Specify Paths
+app.use('/PCF', PCFRoutes);
+app.use('/metrics/', Metrics);
+
 //pretty much useless, used it to test db connection
 app.get('/', function(request,response) {
   if(!conn_succ) {
@@ -113,66 +121,6 @@ app.get('/select/table/:table', function(request,response) {
         else {
             response.json(results);
         }
-  });
-});
-
-////////////////////////////////////////////////////
-//                                                //
-//    Metrics API Calls                           //
-//                                                //
-////////////////////////////////////////////////////
-
-app.get('/metrics/total/interns', function(request,response) {
-  connection.query('SELECT COUNT(*) "Count" FROM Members, Role WHERE Members.MemberID = Role.MemberID AND Role.Type = "Intern" AND Role.Status != "Not Active"', function (error, results, fields) {
-    if(error) {
-      response.json({Metrics_get: "failed"});
-    }
-    else {
-      response.json(results);
-    }
-  });
-});
-
-// Manager Dashboard graph data
-app.get('/metrics/recent/semesters', function(request,response) {
-  var totals = {
-    Total1: 0, Total2: 0, Total3: 0, Grad1: 0, Grad2: 0, Grad3: 0
-  }
-
-  connection.query('SELECT SUM(CASE WHEN T.Semester = "FA18" THEN 1 ELSE 0 END) Total3, ' +
-                   'SUM(CASE WHEN T.Semester = "SP19" THEN 1 ELSE 0 END) Total2, ' +
-                   'SUM(CASE WHEN T.Semester = "SU19" THEN 1 ELSE 0 END) Total1, ' +
-                   'SUM(CASE WHEN T.Semester = "FA18" AND M.GradSemester = "Fall" AND M.GradYear = "2018" THEN 1 ELSE 0 END) Grad3, ' +
-                   'SUM(CASE WHEN T.Semester = "SP19" AND M.GradSemester = "Spring" AND M.GradYear = "2019" THEN 1 ELSE 0 END) Grad2, ' +
-                   'SUM(CASE WHEN T.Semester = "SU19" AND M.GradSemester = "Summer" AND M.GradYear = "2019" THEN 1 ELSE 0 END) Grad1 ' + 
-                   'FROM Members M, Teams T, TeamMembers TM ' +
-                   'WHERE TM.TeamID = T.TeamID AND M.MemberID = TM.MemberID', function (error, results, fields) {
-    if(error) {
-      response.json({sem_totals: "failed"});
-    }
-    else {
-      console.log({results})
-      
-      totals.Total1 = results[0].Total1;
-      totals.Total2 = results[0].Total2;
-      totals.Total3 = results[0].Total3;
-      totals.Grad1 = results[0].Grad1;
-      totals.Grad2 = results[0].Grad2;
-      totals.Grad3 = results[0].Grad3;
-
-      response.json({totals})
-    }
-  });
-});
-
-app.get('/metrics/total/OH', function(request,response) {
-  connection.query('SELECT COUNT(*) "OpenHouse" FROM Members, Role WHERE Members.MemberID = Role.MemberID AND Role.Type = "Open House"', function (error, results, fields) {
-    if(error) {
-      response.json({Metrics_get: "failed"});
-    }
-    else {
-      response.json(results);
-    }
   });
 });
 
---
+++ routes/Metrics.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var router = express.Router();
+
+////////////////////////////////////////////////////
+//                                                //
+//    Metrics API Calls                           //
+//                                                //
+////////////////////////////////////////////////////
+  
+  // Manager Dashboard graph data
+  router.get('/recent/semesters', function(request,response) {
+    var totals = {
+      Total1: 0, Total2: 0, Total3: 0, Grad1: 0, Grad2: 0, Grad3: 0
+    }
+  
+    connection.query('SELECT SUM(CASE WHEN T.Semester = "FA18" THEN 1 ELSE 0 END) Total3, ' +
+                     'SUM(CASE WHEN T.Semester = "SP19" THEN 1 ELSE 0 END) Total2, ' +
+                     'SUM(CASE WHEN T.Semester = "SU19" THEN 1 ELSE 0 END) Total1, ' +
+                     'SUM(CASE WHEN T.Semester = "FA18" AND M.GradSemester = "Fall" AND M.GradYear = "2018" THEN 1 ELSE 0 END) Grad3, ' +
+                     'SUM(CASE WHEN T.Semester = "SP19" AND M.GradSemester = "Spring" AND M.GradYear = "2019" THEN 1 ELSE 0 END) Grad2, ' +
+                     'SUM(CASE WHEN T.Semester = "SU19" AND M.GradSemester = "Summer" AND M.GradYear = "2019" THEN 1 ELSE 0 END) Grad1 ' + 
+                     'FROM Members M, Teams T, TeamMembers TM ' +
+                     'WHERE TM.TeamID = T.TeamID AND M.MemberID = TM.MemberID', function (error, results, fields) {
+      if(error) {
+        response.json({sem_totals: "failed"});
+      }
+      else {
+        console.log({results})
+        
+        totals.Total1 = results[0].Total1;
+        totals.Total2 = results[0].Total2;
+        totals.Total3 = results[0].Total3;
+        totals.Grad1 = results[0].Grad1;
+        totals.Grad2 = results[0].Grad2;
+        totals.Grad3 = results[0].Grad3;
+  
+        response.json({totals})
+      }
+    });
+  });
+
+  // Total Interns / Open House
+  router.get('/total/interns', function(request,response) {
+    connection.query('SELECT COUNT(*) "Count" FROM Members, Role WHERE Members.MemberID = Role.MemberID AND Role.Type = "Intern" AND Role.Status != "Not Active"', function (error, results, fields) {
+      if(error) {
+        response.json({Metrics_get: "failed"});
+      }
+      else {
+        response.json(results);
+      }
+    });
+  });
+  
+  router.get('/total/OH', function(request,response) {
+    connection.query('SELECT COUNT(*) "OpenHouse" FROM Members, Role WHERE Members.MemberID = Role.MemberID AND Role.Type = "Open House"', function (error, results, fields) {
+      if(error) {
+        response.json({Metrics_get: "failed"});
+      }
+      else {
+        response.json(results);
+      }
+    });
+  });
+
+module.exports = router
