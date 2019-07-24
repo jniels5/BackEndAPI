9eda35b6a92fcde46086697f7ae26304aad68f9b
Jeremy Nielson <jniels5@discover.com>
2019-07-24 08:54:15 -0500

    MAP API broken off into seperate file

--- index.js
+++ index.js
@@ -96,12 +96,14 @@ connection.connect(function(err) {
 });
 
 // Pull in route files
-var PCFRoutes = require('./routes/PCF');
+var MAP = require("./routes/MAP.js");
 var Metrics = require("./routes/Metrics.js");
+var PCFRoutes = require('./routes/PCF.js');
 
 // Specify Paths
-app.use('/PCF', PCFRoutes);
+app.use('/map/', MAP);
 app.use('/metrics/', Metrics);
+app.use('/pcf', PCFRoutes);
 
 //pretty much useless, used it to test db connection
 app.get('/', function(request,response) {
@@ -695,119 +697,6 @@ app.get('/email/Applicants/OH', function(request,response) {
 //  Atlas - Backend API Calls                            //
 //                                                       //
 ///////////////////////////////////////////////////////////
-
-//********************************************
-//  MAP API                                  *
-//********************************************
-
-// Delegations
-
-app.post('/map/insert/delegations/', function(request,response) {
-  //used in connection.query
-  var entry = {
-    Delegator: request.body.Delegator,
-    Email: request.body.Email,
-    Delegatee: request.body.Delegatee,
-    StartDate: request.body.StartDate,
-    EndDate: request.body.EndDate,
-    Description: request.body.Description,
-    State: request.body.State,
-    Status: request.body.Status
-  };
-
-  connection.query('INSERT INTO Delegations set ?', entry, function (error, results, fields) {
-    if(error) {
-        response.json({
-          insert_delegation: "failed",
-          delegation_error: error,
-        });
-    }
-    else {
-        response.json({
-          delegation_status: "success",
-        });
-      }
-    });
-  });
-
-app.post('/map/update/delegations', function(request,response) {
-    // used in connection.query
-    var entry = {
-      Delegator: request.body.Delegator,
-      Email: request.body.Email,
-      Delegatee: request.body.Delegatee,
-      StartDate: request.body.StartDate,
-      EndDate: request.body.EndDate,
-      Description: request.body.Description,
-      State: request.body.State,
-      Status: request.body.Status
-    };
-
-    connection.query('SET foreign_key_checks = 0; ' +
-    'UPDATE Delegations SET ? WHERE DelegationID = ' + request.body.DelegationID + ";" +
-    'SET foreign_key_checks = 1;', entry, function (error, results, fields) {
-      if(error) {
-        response.json({update_delegations: "failed"});
-      }
-      else {
-        response.json({update_delegations: "success"})
-      }
-    });
-  });
-
-// Removes all entries (Deprecated)
-app.get('/map/delete/delegations', function(request,response) {
-    connection.query('DELETE FROM Delegations;', function (error, results, fields) {
-      if(error) {
-        response.json({delete_delegations: "failed"});
-      }
-      else {
-        response.json({delete_delegations: "success"})
-      }
-    });
-  });
-
-// Removes specific entry
-app.get('/map/remove/delegation/:dID', function(request,response) {
-    connection.query('DELETE FROM Delegations WHERE DelegationID = ' + request.params.dID + ';', function (error, results, fields) {
-      if(error) {
-        response.json({remove_delegation: "failed"});
-      }
-      else {
-        response.json({remove_delegation: "success"})
-      }
-    });
-  });
-
-  // Approvals
-
-app.post('/map/insert/approvals/', function(request,response) {
-    //used in connection.query
-    var entry = {
-      Email: request.body.Email,
-      FullName: request.body.FullName,
-      Service: request.body.Service,
-      Description: request.body.Description,
-      RequestedBy: request.body.RequestedBy,
-      RequestedDate: request.body.RequestedDate,
-      CloseDate: request.body.CloseDate,
-      Status: request.body.Status
-    };
-
-    connection.query('INSERT INTO ApprovalsHistory set ?', entry, function (error, results, fields) {
-      if(error) {
-          response.json({
-            approval_status: "failed",
-            approval_error: error,
-          });
-      }
-      else {
-          response.json({
-            approval_status: "success",
-          });
-        }
-      });
-    });
 
 //********************************************
 //  code_orange Website API                  *
