6449179cea6533f7034cba78fcc2fee044af9bbc
Jeremy Nielson <jniels5@discover.com>
2019-07-24 15:33:27 -0500

    Temporary workaround, needs to fix

--- routes/Stats/Lab.js
+++ routes/Stats/Lab.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
+var cors = require('cors');
 
 var connection = require("../../auth/Connect");
-var useCors = require("../../auth/Cors");
 
-useCors();
+var whitelist = [
+  'http://localhost:3000/',
+  'http://team11-frontend.mjhmkfjvi5.us-east-2.elasticbeanstalk.com/'
+]
+
+//keep out the baddies
+var corsOptions = {
+  origin: '*',
+  //origin: whitelist,
+  methods: 'GET,HEAD,POST,OPTIONS,DELETE',
+  "preflightContinue": true
+}
+
+router.use(cors(corsOptions)); 
 
 router.get('/projects', function(request,response) {
     if (request.query.Semester == "unassigned")
