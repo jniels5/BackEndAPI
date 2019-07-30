28baf784345ef9747176c3e5e46428ab23555c69
Jacob Drzewiecki <jacobdrzewiecki@discover.com>
2019-07-30 13:44:33 -0500

    tried implementing promises into access checker

--- auth/AppAccess.js
+++ auth/AppAccess.js
@@ -11,15 +11,41 @@ class AppAccess{
      * @author Jacob Drzewiecki
      * @param {string} key      The AppID of the application trying to access the call. Key will be mysql.escaped in function.
      * @param {number} access   The minimum access level the application must have to access the call
+     * @param {response} resp   The response to allow immediate 403/500
      * @return {boolean}        true if allowed to access call
+     * 
      */
-    async check(key, access)
-    {
-        let query = "SELECT AccessLevel FROM Applications WHERE AppID=" + mysql.escape(key) + ";";
-        console.log(query);
-        console.log(await connection.query(query).RowDataPacket);
-        //return key + ": " + results.AccessLevel + " > " + access + " = " + results.Access > access;
-        //console.log(result);
+    async check(key, access, resp) {
+        var connection = require("../auth/Connect");
+        var retVal;
+        //console.log("creating promise");
+        var p = new Promise(function (resolve, reject) {
+            //console.log("starting promise");
+            let query = "SELECT AccessLevel FROM Applications WHERE AppID=" + mysql.escape(key) + ";";
+            connection.query(query, function (err, results) {
+                //console.log("executing query");
+                if (err) {
+                    //console.log("rejecting promise");
+                    reject(err);
+                }
+                else {
+                    //console.log("resolving promise");
+                    resolve(results[0]);
+                }
+            });
+        });
+        await p.then(function (result) {
+            //console.log("p.then success");
+            //return AccessLevel >= access;
+            console.log(key + ": " + result.AccessLevel + " >= " + access);
+            retVal = result.AccessLevel >= access;
+            
+        }, function (error) {
+            retVal = false;
+        });
+        console.log("retVal", retVal);
+        connection.end();
+        return retVal;
     }
 }
 
