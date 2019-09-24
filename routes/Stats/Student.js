78cd1bb68245016ad5f448718941635ad2808b91
Jeremy Nielson <jniels5@discover.com>
2019-09-24 10:18:32 -0500

    minor tweaks

--- routes/Stats/Student.js
+++ routes/Stats/Student.js
@@ -29,7 +29,7 @@ router.use(bodyParser.json());
 ////////////////////////////////////////////////////
 
 router.get('/portal/info', function(request,response) {
-    let query = 'SELECT m.MemberID, m.FirstName, m.LastName, m.GradSemester, m.GradYear, m.Email, m.AssetID, m.Gender, m.Email, m.WorkEmail, m.SuperUser, ' +
+    let query = 'SELECT m.MemberID, m.FirstName, m.LastName, m.GradSemester, m.GradYear, m.Email, m.AssetID, m.Gender, m.Email, m.PhoneNum, m.WorkEmail, m.SuperUser, ' +
                      't.TeamNumber, t.TeamName, t.Semester, t.PhotoPath, t.LabID, ' +
                      'r.Type, r.Status, r.Description, r.Date, ' +
                      'p.ProjectID, p.Name, p.Description, p.Paragraph, p.FrontEnd, p.BackEnd, p.RDS ' +
@@ -54,12 +54,11 @@ router.get('/portal/info', function(request,response) {
       GradSemester: request.body.GradSemester,
       GradYear: request.body.GradYear,
       Email: request.body.Email,
-      PhoneNum: request.body.PhoneNum,
-      MemberID: request.body.MemberID
+      PhoneNum: request.body.PhoneNum
     };
 
     var projUpdate = {
-      Name: request.body.ProjectName,
+      Name: request.body.Name,
       Paragraph: request.body.Paragraph,
       FrontEnd: request.body.FrontEnd,
       BackEnd: request.body.BackEnd,
@@ -72,16 +71,26 @@ router.get('/portal/info', function(request,response) {
     };
   
     connection.query('SET foreign_key_checks = 0; ' +
-    'UPDATE Members SET ? WHERE MemberID = ' + request.body.MemberID + ";" +
-    'UPDATE Teams SET ? WHERE TeamID = ' + request.body.TeamID + ";" +
-    'UPDATE Projects SET ? WHERE ProjectID = ' + request.body.ProjectID + ";" +
-    'SET foreign_key_checks = 1;', membUpdate, teamUpdate, projUpdate, function (error, results, fields) {
+    'UPDATE Members SET ? WHERE MemberID = ' + request.body.MemberID + ";", membUpdate, function (error) {
       if(error) {
         response.json({student_update: "failed: " + error});
-      } else {
-        respose.json({student_update: "success"})
       }
     });
+
+    connection.query('UPDATE Teams SET ? WHERE TeamID = ' + request.body.TeamID + "; ", teamUpdate, function (error) {
+        if(error) {
+          response.json({student_update: "failed: " + error});
+        }
+      });
+
+      connection.query('UPDATE Projects SET ? WHERE ProjectID = ' + request.body.ProjectID + "; " +
+        'SET foreign_key_checks = 1;', projUpdate, function (error) {
+          if(error) {
+            response.json({student_update: "failed: " + error});
+          } else {
+            respose.json({student_update: "success"})
+          }
+        });
   });
 
 module.exports = router
